ef5b820f39e436a210c5908272f2a034d111b568
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-12-10 19:20:46 -1000

    first set of tests complete

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -7,9 +7,41 @@
 const should = chai.should();
 const expect = chai.expect();
 
-describe('shoppingListItem', ()=>{
-  let ListItem = new ShoppingListItem;
+describe('ShoppingListItem', ()=>{
+  let listItem;
+
+  beforeEach(()=>{
+    listItem = new ShoppingListItem('test');
+  });
+
+
   it('should be a class', ()=>{
     ShoppingListItem.should.be.a('function');
   });
+
+  it('should have a property named name', ()=>{
+    listItem.should.have.property('name');
+  });
+
+  it('should have property named description', ()=>{
+    listItem.should.have.property('description');
+  });
+
+  it('should have property named is_done', ()=>{
+    listItem.should.have.property('is_done');
+  });
+
+  it('should have a method named check', ()=>{
+    listItem.should.have.method('check');
+  });
+
+  it('should have a method named uncheck', ()=>{
+    listItem.should.have.method('uncheck');
+  });
+
+  it('should have a method named render', ()=>{
+    listItem.should.have.method('render');
+  });
+
+
 });
