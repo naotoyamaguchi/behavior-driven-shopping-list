eda9c7bef30d4be101d023b42f6be3e218615cd3
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-12-10 18:48:26 -1000

    got the skeleton layed out:

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,1 +1,15 @@
-console.log("TEST Me")
+// jshint esversion: 6
+
+// console.log('mocha', mocha);
+// console.log('chai', chai);
+// console.log('describe', describe);
+
+const should = chai.should();
+const expect = chai.expect();
+
+describe('shoppingListItem', ()=>{
+  let ListItem = new ShoppingListItem;
+  it('should be a class', ()=>{
+    ShoppingListItem.should.be.a('function');
+  });
+});
