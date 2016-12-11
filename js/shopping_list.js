4940a1013a69c55ad0c360ca5a4fad6252c7956d
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-12-11 12:48:17 -1000

    added functions to shoppingList

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -5,16 +5,34 @@ class ShoppingList {
     this.items = [];
   }
 
-  addItem(ShoppingListItem) {
-
+  addItem(item) {
+    if(item instanceof ShoppingListItem){
+      this.items.push(item);
+    } else {
+      throw err;
+    }
   }
 
-  removeItem() {
-  
+  removeItem(item) {
+    if(this.items.indexOf(item) > -1 && item instanceof ShoppingListItem){
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+    if(item === undefined && this.items.length > 0){
+      this.items.pop();
+    } else {
+      throw err;
+    }
+
   }
 
   render() {
-  
+    var listHTML = document.createElement('ul');
+    for(let i = 0; i < this.items.length; i++){
+      var listItem = document.createElement('li');
+      listItem.innerHTML = this.items[i];
+      listHTML.appendChild(listItem);
+    }
+    return listHTML.outerHTML;
   }
 
 }
